21adb5d848f91b7fd3e32c9e579f1e33ca600ce6
aflinchb <anflinch@microsoft.com>
2020-01-09 18:13:11 -0600

    Updating to version 0.5.0 (#25)

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -8,6 +8,7 @@ export const appInsightsKey = "AppInsightsKey";
 export const portConstant = "4120";
 
 export const webInstanceRole = "WEBSITE_ROLE_INSTANCE_ID";
+export const version = process.env.npm_package_version;
 
 export const defaultPageSize = 100;
 export const maxPageSize = 1000;
