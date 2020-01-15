e65d58534385b766a83ed97ac57895a76ed1d69f
Anne Flinchbaugh <anflinch@microsoft.com>
2020-01-15 20:16:57 +0000

    Initializing cosmos provider at app start

--- src/db/idatabaseprovider.ts
+++ src/db/idatabaseprovider.ts
@@ -1,5 +1,11 @@
 export interface IDatabaseProvider {
     /**
+     * Initialize the Cosmos DB Container.
+     * This is handled in a separate method to avoid calling async operations in the constructor.
+     */
+    initialize();
+
+    /**
      * Runs the given query against CosmosDB.
      * @param query The query to select the documents.
      */
