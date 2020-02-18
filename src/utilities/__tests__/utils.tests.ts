5384ec81705db94821514553e0e05f9559577436
Tobias Wright <tobias.wright@gmail.com>
2020-02-18 17:07:48 -0500

    exculded adding test to dist folder (#55)
    * exculded adding test to dist folder
    
    * fixed linting issues
    
    * added check for NaN
    
    * added test, cleaned up queryfunc
    
    * corrected typo in comments
    
    * added exclusion of test files for build in dockerCI

--- src/utilities/__tests__/utils.tests.ts
+++ src/utilities/__tests__/utils.tests.ts
@@ -1,5 +1,27 @@
 import { QueryUtilities } from "../queryUtilities";
+import { DateUtilities } from "../dateUtilities";
+import { IsEqualToProperty } from "../validationUtilities";
+import { VersionUtilities } from "../versionUtilities";
 
 test("Get partition key", () => {
+  expect(QueryUtilities.getPartitionKey("nm1234")).toEqual(expect.any(String));
+  expect(QueryUtilities.getPartitionKey("nm1234")).toBe("4");
   expect(QueryUtilities.getPartitionKey("tt1234")).toBe("4");
+  expect(QueryUtilities.getPartitionKey("tttttt")).toBe("0");
+  expect(QueryUtilities.getPartitionKey("Action")).toBe("0");
+  expect(QueryUtilities.getPartitionKey("tt1")).toBe("0");
 });
+
+test("Get Timings", () => {
+  expect(DateUtilities.getTimer()).toEqual(expect.any(Function));
+  expect(DateUtilities.getDurationMS([ 1800216, 25 ])).toEqual(expect.any(String));
+  expect(DateUtilities.getDurationMS([ 1800216, 25 ])).toBe("1800216000");
+});
+
+test("Get Validation", () => {
+  expect(IsEqualToProperty("hello")).toEqual(expect.any(Function));
+ });
+
+test("Get Build Version", () => {
+  expect(VersionUtilities.getBuildVersion()).toEqual(expect.any(String));
+ });
