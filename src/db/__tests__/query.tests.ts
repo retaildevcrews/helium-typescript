007493e8e83ee30a8d95de560daef81659f7d783
tobiaswright <tobias.wright@gmail.com>
2020-03-02 02:08:21 -0500

    added test suite to db quries

---
+++ src/db/__tests__/query.tests.ts
@@ -0,0 +1,148 @@
+import "reflect-metadata";
+import { CosmosClient, Container, FeedOptions } from "@azure/cosmos";
+import { CosmosDBProvider } from "../cosmosdbprovider";
+
+jest.mock("@azure/cosmos");
+
+test("Check cosmos client dependency", () => {
+    const query = new CosmosDBProvider("http://www.db.com", "dbkey", "db", "collection", null);
+
+    expect(CosmosClient).toHaveBeenCalledTimes(1);
+});
+
+describe("Query Actors", () => {
+    test("Expected output", () => {
+        const query = new CosmosDBProvider("http://www.db.com", "dbkey", "db", "collection", null);
+        query.queryDocuments = jest.fn().mockResolvedValue(["Lawrence Fishburn"]);
+
+        expect(query.queryActors({})).resolves.toEqual(["Lawrence Fishburn"]);
+    });
+
+    describe("Expected SQL statement given the argument", () => {
+
+        const query = new CosmosDBProvider("http://www.db.com", "dbkey", "db", "collection", null);
+        query.queryDocuments = jest.fn();
+
+        test("Defualt params sent to DB", () => {
+            query.queryActors({});
+            const call = "select m.id, m.partitionKey, m.actorId, m.type, m.name, m.birthYear, m.deathYear, m.profession, m.textSearch, m.movies from m where m.type = 'Actor'  order by m.name offset 0 limit 100 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with Actor", () => {
+            query.queryActors({q: "Lawrence Fisburn"});
+            const call = "select m.id, m.partitionKey, m.actorId, m.type, m.name, m.birthYear, m.deathYear, m.profession, m.textSearch, m.movies from m where m.type = 'Actor'  and contains(m.textSearch, 'lawrence fisburn') order by m.name offset 0 limit 100 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with PageSize and PageNumber", () => {
+            query.queryActors({pageSize: 20, pageNumber: 20});
+            const call = "select m.id, m.partitionKey, m.actorId, m.type, m.name, m.birthYear, m.deathYear, m.profession, m.textSearch, m.movies from m where m.type = 'Actor'  order by m.name offset 380 limit 20 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with PageSize < 0", () => {
+            query.queryActors({pageSize: 0, pageNumber: 1});
+            const call = "select m.id, m.partitionKey, m.actorId, m.type, m.name, m.birthYear, m.deathYear, m.profession, m.textSearch, m.movies from m where m.type = 'Actor'  order by m.name offset 0 limit 100 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with PageSize > maxPageSize", () => {
+            query.queryActors({pageSize: 1001, pageNumber: 20});
+            const call = "select m.id, m.partitionKey, m.actorId, m.type, m.name, m.birthYear, m.deathYear, m.profession, m.textSearch, m.movies from m where m.type = 'Actor'  order by m.name offset 19000 limit 1000 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with PageNumber <  0", () => {
+            query.queryActors({pageSize: 1001, pageNumber: -20});
+            const call = "select m.id, m.partitionKey, m.actorId, m.type, m.name, m.birthYear, m.deathYear, m.profession, m.textSearch, m.movies from m where m.type = 'Actor'  order by m.name offset 0 limit 1000 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+    });
+
+});
+
+describe("Query Movies", () => {
+    test("Expected output", () => {
+        const query = new CosmosDBProvider("http://www.db.com", "dbkey", "db", "collection", null);
+        query.queryDocuments = jest.fn().mockResolvedValue(["The Matrix"]);
+
+        expect(query.queryMovies({})).resolves.toEqual(["The Matrix"]);
+    });
+
+    describe("Expected SQL statement given the argument", () => {
+        const query = new CosmosDBProvider("http://www.db.com", "dbkey", "db", "collection", null);
+        query.queryDocuments = jest.fn();
+
+        test("Defualt params sent to DB", () => {
+            query.queryMovies({});
+            const call = "select m.id, m.partitionKey, m.movieId, m.type, m.textSearch, m.title, m.year, m.runtime, m.rating, m.votes, m.totalScore, m.genres, m.roles from m where m.type = 'Movie'  order by m.title offset 0 limit 100 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with pageSize < 0", () => {
+            query.queryMovies({pageSize: 0, pageNumber: 20});
+            const call = "select m.id, m.partitionKey, m.movieId, m.type, m.textSearch, m.title, m.year, m.runtime, m.rating, m.votes, m.totalScore, m.genres, m.roles from m where m.type = 'Movie'  order by m.title offset 1900 limit 100 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with pageSize > maxPageSize", () => {
+            query.queryMovies({pageSize: 1001, pageNumber: 20});
+            const call = "select m.id, m.partitionKey, m.movieId, m.type, m.textSearch, m.title, m.year, m.runtime, m.rating, m.votes, m.totalScore, m.genres, m.roles from m where m.type = 'Movie'  order by m.title offset 19000 limit 1000 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with pageNumber < 0", () => {
+            query.queryMovies({pageSize: 1001, pageNumber: -20});
+            const call = "select m.id, m.partitionKey, m.movieId, m.type, m.textSearch, m.title, m.year, m.runtime, m.rating, m.votes, m.totalScore, m.genres, m.roles from m where m.type = 'Movie'  order by m.title offset 0 limit 1000 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with a query", () => {
+            query.queryMovies({q: "The Squel"});
+            const call = "select m.id, m.partitionKey, m.movieId, m.type, m.textSearch, m.title, m.year, m.runtime, m.rating, m.votes, m.totalScore, m.genres, m.roles from m where m.type = 'Movie'  and contains(m.textSearch, 'the squel')  order by m.title offset 0 limit 100 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with a year", () => {
+            query.queryMovies({year: "1998"});
+            const call = "select m.id, m.partitionKey, m.movieId, m.type, m.textSearch, m.title, m.year, m.runtime, m.rating, m.votes, m.totalScore, m.genres, m.roles from m where m.type = 'Movie'  and m.year = 1998  order by m.title offset 0 limit 100 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with a rating", () => {
+            query.queryMovies({rating: 5});
+            const call = "select m.id, m.partitionKey, m.movieId, m.type, m.textSearch, m.title, m.year, m.runtime, m.rating, m.votes, m.totalScore, m.genres, m.roles from m where m.type = 'Movie'  and m.rating >= 5  order by m.title offset 0 limit 100 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with a toprated", () => {
+            query.queryMovies({toprated: true});
+            const call = "select top 10 m.id, m.partitionKey, m.movieId, m.type, m.textSearch, m.title, m.year, m.runtime, m.rating, m.votes, m.totalScore, m.genres, m.roles from m where m.type = 'Movie'  order by m.rating desc";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+        test("Params with a actorID", () => {
+            query.queryMovies({actorId: "nm0000401"});
+            const call = "select m.id, m.partitionKey, m.movieId, m.type, m.textSearch, m.title, m.year, m.runtime, m.rating, m.votes, m.totalScore, m.genres, m.roles from m where m.type = 'Movie'  order by m.title offset 0 limit 100 ";
+
+            expect(query.queryDocuments).toBeCalledWith(call);
+        });
+
+    });
+
+});
