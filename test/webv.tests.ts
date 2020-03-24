c254f7d338f226c1149fd33596c0ee9aace1f319
Jeremy Foster <jeremy.foster@live.com>
2020-03-23 21:40:25 -0700

    intermediate commit (coverage at 83% with webv)

--- test/webv.tests.ts
+++ test/webv.tests.ts
@@ -1,4 +1,4 @@
-// import { exec } from "child_process";
+import { exec } from "child_process";
 
 process.env.KeyVaultName = "cfhe";
 process.env.AUTH_TYPE = "CLI";
@@ -8,19 +8,20 @@ import { HeliumServer } from "../src/server";
 const server = new HeliumServer();
 
 describe("Web Validate", async () => {
-    await server.initialize();
+    server.initialize();
     // this test requires that you have the helium/webvalidate project in a directory next to helium-typescript
-    // it("Run webv against the running server", () => {
-    //     const URL = "localhost:4120";
-    //     const FILES = "node.json baseline.json and bad.json";
-    //     // assert.isTrue(true);
-    //     console.log("executing...");
-    //     exec(`dotnet run  -- --host ${URL} --files ${FILES}`, {
-    //         cwd: "../webvalidate/src/app"
-    //     }, (err, stdout, stderr) => {
-    //         if (err) console.error(err);
-    //         console.log(`stdout: ${stdout}`);
-    //     });
-    // })
-    server.stop();
+    it("Run webv against the running server", () => {
+        const URL = "localhost:4120";
+        const FILES = "node.json baseline.json bad.json";
+
+        console.log("executing...");
+        exec(`dotnet run  -- --host ${URL} --files ${FILES}`, {
+            cwd: "../webvalidate/src/app"
+        }, (err, stdout, stderr) => {
+            console.log("Finished with webv test run");
+            if (err) console.error(`Error running webv: ${err}`);
+            console.log(`stdout: ${stdout}`);
+            server.stop();
+        });
+    })
 });
