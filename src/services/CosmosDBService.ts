630f0d549c644d4e0b3c4cc627687692da1f2743
aflinchb <anflinch@microsoft.com>
2020-03-25 16:45:19 +0000

    Remove try/catch from cosmos db service

--- src/services/CosmosDBService.ts
+++ src/services/CosmosDBService.ts
@@ -54,13 +54,8 @@ export class CosmosDBService {
      * @param query The query to select the documents.
      */
     public async queryDocuments(query: string): Promise<any> {
-        try {
-            const { resources: queryResults } = await this.cosmosContainer.items.query(query, this.feedOptions).fetchAll();
-            return queryResults;
-        } catch (err) {
-            this.logger.error(Error(err), err);
-            throw Error(err);
-        }
+        const { resources: queryResults } = await this.cosmosContainer.items.query(query, this.feedOptions).fetchAll();
+        return queryResults;
     }
 
     /**
@@ -68,19 +63,14 @@ export class CosmosDBService {
      * @param documentId The id of the document to query.
      */
     public async getDocument(documentId: string): Promise<any> {
-        try {
-            const { resource: result, statusCode: status }
-                = await this.cosmosContainer.item(documentId, QueryUtilities.getPartitionKey(documentId)).read();
-            if (status === 200) {
-                return result;
-            }
-
-            throw Error("Cosmos Error: " + status);
-
-        } catch (err) {
-            this.logger.error(Error(err), err);
-            throw err;
+        const { resource: result, statusCode: status }
+            = await this.cosmosContainer.item(documentId, QueryUtilities.getPartitionKey(documentId)).read();
+        if (status === 200) {
+            return result;
         }
+        
+        // 404 not found does not throw an error
+        throw Error("Cosmos Error: " + status);
     }
 
     /**
