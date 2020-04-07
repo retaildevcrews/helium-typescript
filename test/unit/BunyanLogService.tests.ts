a90226ca4a603356cbea221d59aa5efe00b64162
Tobias Wright <tobias.wright@gmail.com>
2020-04-07 12:57:58 -0400

    updates to test, merged master

--- test/unit/BunyanLogService.tests.ts
+++ test/unit/BunyanLogService.tests.ts
@@ -1,22 +1,20 @@
 import "reflect-metadata";
 import { BunyanLogService } from "../../src/services";
 import * as bunyan from "bunyan";
-const chai = require("chai");
-const sinon = require("sinon");
-const sinonChai = require("sinon-chai");
+import * as chai from  "chai";
+import * as sinon from "sinon";
+import * as sinonChai  from "sinon-chai";
 
 chai.should();
 chai.use(sinonChai);
 
 describe("BunyanLogService", () => {
-  const spy = sinon.spy(bunyan, 'createLogger');
+  const spy = sinon.spy(bunyan, "createLogger");
   const log = new BunyanLogService;
 
   describe("trace", () => {
     it("Bunyan is initiated", () => {
-      
       log.trace("Can you hear me now?");
-      console.log(process.stdout.write)
       spy.should.have.been.calledOnce;
     });
   });
