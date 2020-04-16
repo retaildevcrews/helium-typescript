47235863a2c66d90b2353dd4ec79dc658089db4b
Tobias Wright <tobias.wright@gmail.com>
2020-04-16 09:08:51 -0400

    updates based on comments

--- src/config/ConsoleController.ts
+++ src/config/ConsoleController.ts
@@ -23,7 +23,7 @@ export class ConsoleController {
         else if (validationMessages.length > 0) {
             validationMessages.forEach(m => console.error(m));
             this.showHelp();
-            process.exit();
+            process.exit(-1);
         }
 
         this.logService.setLogLevel(values["log-level"]);
@@ -61,7 +61,7 @@ export class ConsoleController {
             args = commandLineArgs(options);
         } catch(e) {
             this.showHelp("Error: " + e.name);
-            process.exit();
+            process.exit(-1);
         }
         
         // compose the two
